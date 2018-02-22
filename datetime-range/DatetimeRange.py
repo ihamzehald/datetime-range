1807aedbe97f2ff232953df3e0c71531171a9ce5
ihamzehald <h.darawsheh@sit-mena.com>
2018-02-22 18:40:42 +0200

    init

---
+++ datetime-range/DatetimeRange.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+class DatetimeRange:
+
+    def get_date_range(start_date, end_date):
+        return None
+
+    def get_date_range_forward(date, days):
+        return None
+
+    def get_date_range_backword(date, days):
+        return None
+
+    def get_day_range(start_hour, end_hour):
+        return None
+
+    def get_day_hours(date):
+        return None
+
+    def get_day_minutes(date):
+        return None
+
+    def get_day_seconds(date):
+        return None
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='periodic_utils',
+    version='0.0.1',
+    description='Datetime range library',
+    license='MIT',
+    packages=['datetime-range'],
+    author='Hamzeh Al Darawsheh',
+    author_email='ihamzehald@gmail.com',
+    keywords=['datetime','timedelta', 'date range', 'time range', 'date', 'time'],
+    url='https://github.com/ihamzehald'
+)
